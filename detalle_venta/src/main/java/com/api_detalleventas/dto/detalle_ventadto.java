568b11c635c4524a53ef8813c44cb4ff7f235e8b
benjatussy <benja.contrerasg@duocuc.cl>
2025-06-14 17:03:56 -0400

    creado dto

--- detalle_venta/src/main/java/com/api_detalleventas/dto/detalle_ventadto.java
+++ detalle_venta/src/main/java/com/api_detalleventas/dto/detalle_ventadto.java
@@ -1,7 +1,16 @@
 package com.api_detalleventas.dto;
 
+import lombok.*;
 
-
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class detalle_ventadto {
+
+    private Integer id_detalle_venta;
+    private Integer id_venta;
+    private Integer id_producto;
+    private Integer cantidad;
+    private Double precio_unitario;
 
 }
