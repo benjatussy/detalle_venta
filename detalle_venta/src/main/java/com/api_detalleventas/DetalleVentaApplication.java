5e043d83819bebc9c9d124483f80f3d50bb4fe0a
benjatussy <benja.contrerasg@duocuc.cl>
2025-06-14 16:47:10 -0400

    creado repositorio detalle venta

---
+++ detalle_venta/src/main/java/com/api_detalleventas/DetalleVentaApplication.java
@@ -0,0 +1,13 @@
+package com.api_detalleventas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DetalleVentaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DetalleVentaApplication.class, args);
+	}
+
+}
---
+++ detalle_venta/src/test/java/com/api_detalleventas/detalle_venta/DetalleVentaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api_detalleventas.detalle_venta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DetalleVentaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
